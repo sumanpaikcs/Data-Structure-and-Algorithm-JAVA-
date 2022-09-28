344fdd6126e4e02b859a05a9865ca8e1968a8afd
Suman Paik <sumanpaikcs@gmail.com>
2022-09-28 20:15:02 +0530

    linear search

---
+++ Linear Search/PracticeLInearSearch.java
@@ -0,0 +1,16 @@
+public class PracticeLInearSearch {
+    public static void main(String[] args) {
+        int a = {12,34,45,56,76,543,23,90};
+        int b = 76;
+        int answer = linearsearch(b, a);
+    }
+    public static int linearsearch(int target, int ArrayItem){
+        for (int i = 0; i < ArrayItem.length; i++) {
+            if (target == ArrayItem[i]) {
+                System.out.println();
+            }
+            
+        }
+    }
+    
+}
